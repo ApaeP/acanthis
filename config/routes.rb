5b91ff8a3daa51ee5eb638a6f2246448ff183b83
Paul Portier <paulportier@gmail.com>
2020-01-18 19:16:31 +0100

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :antiques
+  get '/infos', to: 'pages#infos'
+  get '/contact', to: 'pages#contact'
 end
