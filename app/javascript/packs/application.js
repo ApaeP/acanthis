7a16babea64e9736468b8877a10f747511c9ea22
Paul Portier <paulportier@gmail.com>
2021-02-22 20:30:54 +0100

    change js for dropdown menus, change crop at loading for cloudinary, use image assets for generic instead of cloudinary, refacto a little

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,36 +6,35 @@ import 'bootstrap';
 
 import { closeShowModalWhenModify } from '../pages/index';
 import { launchFunctionOnLoad } from '../personalfunctions/launchonload';
-import { dropdownmenuPhoto, dropdownmenuInfos } from '../components/dropdownmenus';
 import { openCarouselOnClickedElement, navigateFSCarouselWithArrows } from '../components/fullscreencarousel';
 import { passSrcToVideoModal } from '../components/videomodal';
-import { initSwiperHome } from '../components/homecarousel'; //, resizeSwiperElements } from '../components/homecarousel';
+import { initSwiperHome } from '../components/homecarousel';
 import { initMap } from '../components/googlemap.js.erb';
+import { openCloseDropdownMenu } from '../components/dropdown_menus';
+// import { dropdownmenuPhoto, dropdownmenuInfos } from '../components/dropdownmenus';
+// resizeSwiperElements } from '../components/homecarousel';
 
 $(document).on('turbolinks:load', function() {
 
-  console.log('coucou1')
+  openCloseDropdownMenu('dropdownPhotoMenuLink', 'nav-dropdown-photo');
+  openCloseDropdownMenu('dropdownMenuLinkInfos', 'nav-dropdown-infos');
   initMap();
   $(".dropdown-toggle").dropdown();
   $('#carouselFullscreen').carousel('pause')
   navigateFSCarouselWithArrows();
-  // resizeSwiperElements();
-  dropdownmenuPhoto();
-  dropdownmenuInfos();
   closeShowModalWhenModify();
   passSrcToVideoModal();
   openCarouselOnClickedElement();
   initSwiperHome();
-  // var fsCarouselHeight = $('.photo-fs-carousel').width();
-  console.log('coucou2')
-  // $('.fs-car-item').css({
-  //     'max-height': fsCarouselHeight + 'px'
-  // });
-});
 
-document.querySelectorAll('.submit-new-element').forEach((btn) => {
-  btn.addEventListener('click', (event) => {
-    if (document.querySelector('#antique_title').value !== "")
-    document.querySelector('#loading-new-elem').style.display = 'block'
+  // dropdownmenuPhoto();
+  // dropdownmenuInfos();
+  // resizeSwiperElements();
+  document.querySelectorAll('.submit-new-element').forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      if (document.querySelector('#antique_title').value !== "")
+      document.querySelector('#loading-new-elem').style.display = 'block'
+    });
   });
 });
+
