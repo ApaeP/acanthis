d2b7c68bef3740a24532ec6eaf9fbda5b588da91
Paul Portier <paulportier@gmail.com>
2021-02-23 14:41:02 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root to: 'pages#home'
+  devise_scope :user do
+   get "change_password", to: "devise/registrations#edit", as: :edit_user_registration
+   patch "users", to: "devise/registrations#update", as: :user_registration
+   put "users", to: "devise/registrations#update"
+  end
   devise_for :users, :skip => [:registrations]
   resources :categories, only: [:create, :update, :destroy]
   resources :antiques, only: [:index, :create, :update, :destroy]
