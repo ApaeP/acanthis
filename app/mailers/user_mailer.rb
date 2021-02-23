24834d7635c8aaabbefcb73cad1979d05091c904
Paul Portier <paulportier@gmail.com>
2021-02-23 16:50:36 +0100

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,14 @@ class UserMailer < ApplicationMailer
   def contact_email_notif_for_user
     @contact = Contact.new
     @contact = params[:contact]
-    mail(to: @contact.email, subject: "Acanthis - Prise de contact effectuée")
+    mail(to: @contact.email, subject: "Acanthis - Votre demande de contact")
   end
 
   def contact_email
     @contact = Contact.new
     @contact = params[:contact]
-    mail(to: 'acanthisdev@gmail.com', subject: "(n°#{@contact.id}) Acanthis - Demande de contact par #{@contact.name}")
+    mail(to: 'acanthisdev@gmail.com', subject: "Acanthis - Demande de contact par #{@contact.name} (n°#{@contact.id})")
+    mail(to: 'acanthis.21@free.fr', subject: "Acanthis - Demande de contact par #{@contact.name} (n°#{@contact.id})")
   end
 end
 
