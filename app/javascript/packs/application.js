b73289fa815383ed4acbfeee5311ff92538f7539
Paul Portier <paulportier@gmail.com>
2021-02-26 03:34:52 +0100

    removed unused files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,19 +5,14 @@ require("channels")
 import 'bootstrap';
 
 import { closeShowModalWhenModify }                                   from '../pages/index';
-import { launchFunctionOnLoad }                                       from '../personalfunctions/launchonload';
 import { openCarouselOnClickedElement, navigateFSCarouselWithArrows } from '../components/fullscreencarousel';
 import { passSrcToVideoModal }                                        from '../components/videomodal';
 import { initSwiperHome }                                             from '../components/homecarousel';
 import { initMap }                                                    from '../components/googlemap';
-import { openCloseDropdownMenu }                                      from '../components/dropdown_menus';
 
 $(document).on('turbolinks:load', function() {
 
-  openCloseDropdownMenu('dropdownPhotoMenuLink', 'nav-dropdown-photo');
-  openCloseDropdownMenu('dropdownMenuLinkInfos', 'nav-dropdown-infos');
   initMap();
-  $(".dropdown-toggle").dropdown();
   $('#carouselFullscreen').carousel('pause');
   navigateFSCarouselWithArrows();
   closeShowModalWhenModify();
