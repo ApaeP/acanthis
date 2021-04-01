69fa42a8d137c91d789c2f7f4e14cb40b9d231de
Paul Portier <paulportier@gmail.com>
2021-04-02 00:01:34 +0200

    ready to merge (missing color fixing)

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,10 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :antique
+
+  after_commit :delete_attachments
+
+  def delete_attachments
+    self.antique.photo.purge if self.antique.photo.attached?
+  end
 end
